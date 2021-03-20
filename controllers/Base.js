9b66b6f717427c8fbb566278e0b00bba14d1845e
Zdravko <zdravko.sokcevic@itcentar.rs>
2021-03-20 17:59:28 +0100

    /

--- controllers/Base.js
+++ controllers/Base.js
@@ -1,11 +1,68 @@
 const Base = {};
 const Request = require('./Request');
+const Parser = require('./Parser');
+const config = require('../api/config');
 Base.search = (req,res) => {
 	let url = req.body.search;
-	console.log(url);
-	Request.request(url, {},'', 'GET').then(data=> {
-		res.end(JSON.stringify({data:data}));
+
+	const parsed = Parser.parseInput(url);
+	let allData = [];
+	parsed.forEach( item=> {
+		for(let x=0;x<config.length; x++) {
+			let api = config[x];
+			let results = {};
+			data = Base.correctRequest(item, api);
+
+			let baseUrl = api.url;
+			let data = api.data ?? {};
+			let returnType = api.returnType;
+			let method = api.method;
+			let headers = api.headers ?? {};
+			Request.request(baseUrl, data,returnType, method, headers).then(data=> {
+				results.push(eval(Parser.api['parseMethod'], data))
+			})
+		}
+		allData.push({
+			url: item,
+			resulst: results
+		})
 	})
+}
+
+Base.correctRequest = (item, api) => {
+	if(item == false)
+		return;
+	let value = item.value;
+	let type = item.type;
+	if('apiKey' in api && api.type=='GET') {
+		api.url.prototype.replace('%%apiKey%%', api.apiKey);
+	}
+	if(api.url.includes('%%url%%'))
+	{
+		// Check if url is url
+		// if isUrl
+		if(type == 'URL') {
+			api.url.prototype.replace('%%url%%', value);
+		}else {
+			// correct ip to url
+			let url = Parser.ipToDomain(value);
+			api.url.prototype.replace('%%url%%', url);
+		}
+	}else if(api.url.includes('%%ip%%')) {
+		// if is IP
+		// if is
+		if(type == 'URL') {
+			let ip = Parser.domainToIp(value);
+			api.url.prototype.replace('%%ip%%', ip);
+		}else {
+			api.prototype.replace('%%ip%%', value);
+		}
+	}
+
+	if(api.type == 'POST') {
+
+	}
+	return api;
 }
 
 module.exports = Base;
--- controllers/Parser.js
+++ controllers/Parser.js
@@ -1,6 +1,7 @@
 const dns = require('dns');
+const Parser = {};
 
-const domainToIp = domain => {
+Parser.domainToIp = domain => {
 	return new Promise((res,rej) => {
 		dns.lookup('example.org', (err, address, family) => {
 			res(address);
@@ -10,7 +11,7 @@ const domainToIp = domain => {
 	})
 }
 
-const ipToDomain = (ip, port=80) => {
+Parser.ipToDomain = (ip, port=80) => {
 	return new Promise((res,rej) => {
 		const dnsPromises = dns.promises;
 		dnsPromises.lookupService(ip, 80).then((result, domain) => {
@@ -21,7 +22,7 @@ const ipToDomain = (ip, port=80) => {
 	})
 }
 
-function reverseLookup(ip) {
+Parser.reverseLookup(ip) {
 	dns.reverse(ip,function(err,domains){
 		if(err!=null)	callback(err);
 
@@ -34,18 +35,31 @@ function reverseLookup(ip) {
 	});
 }
 
-domainToIp('http://google.com');
-const findDoamin = async ip => {
-	let result = await ipToDomain(ip);
-	console.log(result);
+// domainToIp('http://google.com');
+// const findDoamin = async ip => {
+// 	let result = await ipToDomain(ip);
+// 	console.log(result);
+// }
+
+// ipToDomain('89.216.25.22').then(res => {
+// 	console.log(res);
+// })
+
+Parser.csvParser = data => {
+
 }
 
-ipToDomain('89.216.25.22').then(res => {
-	console.log(res);
-})
 
-const csvParser = data => {
+Parser.parseInput = input => {
+	let data = '';
+	data = input.split(/\n|,| /);
+	
+	return data;
+}
+
+Parser.ipQualityScoreParser = data => {
 
 }
 
 // address: "93.184.216.34" family: IPv4
+module.exports = Parser;
